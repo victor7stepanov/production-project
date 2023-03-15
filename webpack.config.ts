448dc19fbea47f5249a73eed941a3ee424a14859
Victor <victor7stepanov@ya.ru>
2023-03-15 16:39:43 +0400

    add react and sass loader

--- webpack.config.ts
+++ webpack.config.ts
@@ -2,9 +2,9 @@ import {buildWebpackConfig} from "./config/build/buildWebpackConfig";
 import path from "path";
 import {BuildEnv, BuildPaths} from "./config/build/types/config";
 
-export default (env: BuildEnv) => {
+export default function(env: BuildEnv) {
     const paths: BuildPaths = {
-        entry: path.resolve(__dirname, 'src', 'index.ts'),
+        entry: path.resolve(__dirname, 'src', 'index.tsx'),
         build: path.resolve(__dirname, 'build'),
         html: path.resolve(__dirname, 'public', 'index.html'),
     }
