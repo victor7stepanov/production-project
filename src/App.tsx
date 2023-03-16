f668f4711840ec74e1ee66e82eeb8bfacbb60efe
Victor <victor7stepanov@ya.ru>
2023-03-16 13:28:10 +0400

    add global styles in index.scss and options for isolated class names

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,9 @@
 import {Counter} from "./components/Counter";
+import "./index.scss";
 
 export const App = () => {
     return (
-        <div>
+        <div className='app'>
             <Counter />
         </div>
     );
