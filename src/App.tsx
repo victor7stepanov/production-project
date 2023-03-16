8e96644e63f449ab1b5be5af5a1dea51c7721cc0
Victor <victor7stepanov@ya.ru>
2023-03-16 23:04:07 +0400

    add theme styles and button

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,23 @@ import "./styles/index.scss";
 import {Route, Routes, Link} from "react-router-dom";
 import {AboutPageAsync} from "./pages/AboutPage/AboutPage.async";
 import {MainPageAsync} from "./pages/MainPage/MainPage.async";
-import {Suspense} from "react";
+import {Suspense, useState} from "react";
+
+export enum Theme {
+    LIGHT = 'light',
+    DARK = 'dark'
+}
 
 export const App = () => {
+    const [theme, setTheme] = useState<Theme>(Theme.LIGHT);
+
+    const toggleTheme = () => {
+        setTheme(theme === Theme.LIGHT ? Theme.DARK : Theme.LIGHT);
+    }
+
     return (
-        <div className='app'>
+        <div className={`app ${theme}`}>
+            <button onClick={toggleTheme}>{theme}</button>
             <Link to={'/'}>Главная</Link>
             <Link to={'/about'}>О сайте</Link>
             <Suspense fallback={<div>Loading...</div>}>
