8c7931e42be916f699bcf0ab0c528cdbc03c48b8
Victor <victor7stepanov@ya.ru>
2023-03-20 16:57:34 +0400

    add Button component and user icon

--- src/shared/ui/ThemeSwitcher/ui/ThemeSwitcher.tsx
+++ src/shared/ui/ThemeSwitcher/ui/ThemeSwitcher.tsx
@@ -3,6 +3,7 @@ import cls from "./ThemeSwitcher.module.scss";
 import {Theme, useTheme} from "app/providers/ThemeProvider";
 import LightIcon from "shared/assets/icons/theme-light.svg";
 import DarkIcon from "shared/assets/icons/theme-dark.svg";
+import {Button, ThemeButton} from "shared/ui/Button/Button";
 
 interface ThemeSwitcherProps {
     className?: string;
@@ -12,11 +13,12 @@ export const ThemeSwitcher = ({className}: ThemeSwitcherProps) => {
     const {theme, toggleTheme} = useTheme();
 
     return (
-        <button
+        <Button
+            theme={ThemeButton.CLEAR}
             className={classNames(cls.ThemeSwitcher, {}, [className])}
             onClick={toggleTheme}
         >
             {theme === Theme.LIGHT ? <LightIcon /> : <DarkIcon />}
-        </button>
+        </Button>
     );
 };
